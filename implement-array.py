57c2ee56b5b4f15344e055e8e9a371a6f5b8355b
Xuanlin Wang <32471644+WXLyndon@users.noreply.github.com>
2020-10-07 01:15:16 +0000

    implement-array done

--- implement-array.py
+++ implement-array.py
@@ -6,5 +6,30 @@ class MyArray:
     def get(self, index):
         return self.data[index]
 
+    def push(self, item):
+        self.data.append(item)
+        self.length += 1
+        return self.length
+
+    def pop(self):
+        item = self.data[-1]
+        self.data = self.data[:self.length-1] # python list's index is left closed right open
+        self.length -= 1
+        return item
+    
+    def delete(self, index):
+        item = self.data[index]
+        self.shiftItems(index)
+    
+    def shiftItems(self, index):
+        for i in range(index, self.length-1):
+            self.data[i] = self.data[i + 1]
+        self.data = self.data[:self.length-1]
+        self.length -= 1
+
 newArray = MyArray()
-print(newArray)
+newArray.push('hi')
+newArray.push('you')
+newArray.push('!')
+newArray.delete(1)
+print(newArray.data, newArray.length)
