e49294e50c475775676502ce37e7f41e314699d0
Xuanlin Wang <32471644+WXLyndon@users.noreply.github.com>
2020-10-20 22:27:37 +0000

    Fix a typo in selection_sort.py

--- Sorting/selection_sort.py
+++ Sorting/selection_sort.py
@@ -1,12 +1,12 @@
 def selectionSort(array):
     for i in range(len(array)):
         smallest = array[i]
-        smallest_index = i
+        smallestIndex = i
         for j in range(i+1, len(array)):
             if array[j] < smallest:
                 smallest = array[j]
-                smallest_index = j
-        array[i], array[smallest_index] = smallest,  array[i]
+                smallestIndex = j
+        array[i], array[smallestIndex] = smallest,  array[i]
 
 numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
 selectionSort(numbers)
