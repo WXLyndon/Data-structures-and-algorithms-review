99622426bb67c91088a3ce6ccf8f413377daa1a9
Xuanlin Wang <32471644+WXLyndon@users.noreply.github.com>
2020-10-21 04:54:06 +0000

    init the quick_sort.py

---
+++ Sorting/quick_sort.py
@@ -0,0 +1,9 @@
+def quickSort(array,left,right):
+    return NotImplemented
+
+def partition(array,left,right):
+    return NotImplemented
+
+
+numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
+quickSort(numbers, 0, len(numbers)-1)
