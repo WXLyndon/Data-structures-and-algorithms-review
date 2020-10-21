a2db11de12940a80a4656a226670baa2ee82765d
Xuanlin Wang <32471644+WXLyndon@users.noreply.github.com>
2020-10-21 04:36:10 +0000

    merge_sort.py done

---
+++ Sorting/merge_sort.py
@@ -0,0 +1,27 @@
+def mergeSort(array):
+    if len(array) <= 1:
+        return array
+    mid = len(array) // 2
+    left = mergeSort(array[:mid])
+    right = mergeSort(array[mid:])
+    return merge(left, right)
+
+
+def merge(left, right):
+    l = 0
+    r = 0
+    result = []
+    while l < len(left) and r < len(right):
+        if left[l] <= right[r]:
+            result.append(left[l])
+            l += 1
+        else:
+            result.append(right[r])
+            r += 1
+    result += list(left[l:])
+    result += list(right[r:])
+    return result
+
+
+numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
+print(mergeSort(numbers))
