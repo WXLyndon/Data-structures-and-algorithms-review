e519297ca80a74551ba4116152744d3de1cebc94
Xuanlin Wang <32471644+WXLyndon@users.noreply.github.com>
2020-10-22 04:44:20 +0000

    quick_sort.py done

--- Sorting/quick_sort.py
+++ Sorting/quick_sort.py
@@ -1,9 +1,36 @@
-def quickSort(array,left,right):
-    return NotImplemented
+import random
 
-def partition(array,left,right):
-    return NotImplemented
+
+def quickSort(array, left, right):
+    if left < right:
+        partitionIndex = randomPartion(array, left, right)
+        # partitionIndex = partition(array, left, right)
+        quickSort(array, left, partitionIndex)
+        quickSort(array, partitionIndex + 1, right)
+    else:
+        return
+
+
+def partition(array, left, right):
+    i = left - 1
+    for j in range(left, right):
+        if array[j] <= array[right]:
+            i += 1
+            swap(array, i, j)
+    swap(array, i+1, right)
+    return i
+
+
+def randomPartion(array, left, right):
+    i = random.randint(left, right)
+    swap(array, i, right)
+    return partition(array, left, right)
+
+
+def swap(array, firstIndex, secondIndex):
+    array[firstIndex], array[secondIndex] = array[secondIndex], array[firstIndex]
 
 
 numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
 quickSort(numbers, 0, len(numbers)-1)
+print(numbers)
