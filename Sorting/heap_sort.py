fa8cd51b4c2f621992df129595f3364d0975775f
Xuanlin Wang <32471644+WXLyndon@users.noreply.github.com>
2020-10-25 00:17:02 +0000

    heap_sort.py done

---
+++ Sorting/heap_sort.py
@@ -0,0 +1,30 @@
+def maxHeapify(array, size, i):
+    left = 2 * i + 1
+    right = 2 * i + 2
+    maximum = i
+
+    if left < size and array[i] < array[left]:
+        maximum = left
+
+    if right < size and array[maximum] < array[right]:
+        maximum = right
+
+    if maximum != i:
+        array[i], array[maximum] = array[maximum], array[i]
+        maxHeapify(array, size, maximum)
+
+
+def heapSort(array):
+    size = len(array)
+
+    for i in range(size, -1, -1):
+        maxHeapify(array, size, i)
+
+    for i in range(size-1, 0, -1):
+        array[i], array[0] = array[0], array[i]
+        maxHeapify(array, i, 0)
+
+
+numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
+heapSort(numbers)
+print(numbers)
